9baa753c5c835b49fec4804240115974bdbf9d22
Michael <blondinmichael@gmail.com>
2026-01-04 21:31:51 -0500

    init

--- bioinfo/triangleviewer/static/basic.js
+++
@@ -1,9 +0,0 @@
-
-arr = [1,2,3,4]
-
-
-for (var i = 0; i < 10; i++)
-{
-	arr[i % arr.length] = arr[(i) % arr.length] + arr[(i * i) % arr.length]
-}
-
--- index-v.php
+++
@@ -1,4 +0,0 @@
-<?php
-// affiche le numéro de version courante du PHP.
-echo 'Version PHP courante : ' . phpversion();
-?>
